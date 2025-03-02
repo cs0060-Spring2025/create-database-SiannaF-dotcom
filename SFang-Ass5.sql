daacc4113e5a63e2e9aadcba7217087fa06d6c22
SiannaF-dotcom <siyuan.fang0401@gmail.com>
2025-03-02 11:51:04 -0500

    Add files via upload

---
+++ SFang-Ass5.sql
@@ -0,0 +1,297 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+--
+-- Host: localhost    Database: united_helpers
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `assignment`
+--
+
+DROP TABLE IF EXISTS `assignment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assignment` (
+  `volunteerId` int NOT NULL,
+  `taskCode` int NOT NULL,
+  `startDateTime` datetime NOT NULL,
+  `endDateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`volunteerId`,`taskCode`),
+  KEY `taskCode` (`taskCode`),
+  CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`volunteerId`) REFERENCES `volunteer` (`volunteerId`),
+  CONSTRAINT `assignment_ibfk_2` FOREIGN KEY (`taskCode`) REFERENCES `task` (`taskCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `assignment`
+--
+
+LOCK TABLES `assignment` WRITE;
+/*!40000 ALTER TABLE `assignment` DISABLE KEYS */;
+INSERT INTO `assignment` VALUES (1,1,'2025-03-01 08:00:00','2025-03-01 12:00:00'),(1,2,'2025-03-02 14:00:00','2025-03-02 18:00:00'),(1,3,'2025-03-03 14:00:00','2025-03-03 18:00:00'),(2,1,'2025-03-02 10:00:00','2025-03-02 14:00:00'),(2,2,'2025-03-02 09:00:00','2025-03-04 12:30:00'),(2,3,'2025-03-03 09:00:00','2025-03-04 12:30:00'),(3,3,'2025-03-03 10:00:00','2025-03-03 14:00:00'),(4,4,'2025-03-04 08:30:00',NULL),(5,5,'2025-03-05 09:00:00',NULL),(6,6,'2025-03-06 10:30:00',NULL),(7,7,'2025-03-07 08:00:00','2025-03-08 13:15:00'),(8,8,'2025-03-08 09:15:00','2025-03-08 13:15:00'),(9,9,'2025-03-09 10:00:00','2025-03-09 14:00:00'),(10,10,'2025-03-10 08:45:00','2025-03-10 12:45:00');
+/*!40000 ALTER TABLE `assignment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `itemId` int NOT NULL AUTO_INCREMENT,
+  `itemDescription` varchar(255) NOT NULL,
+  `itemValue` decimal(10,2) NOT NULL,
+  `quantityOnHand` int NOT NULL,
+  PRIMARY KEY (`itemId`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'Bandages',2.50,500),(2,'Antiseptic',3.75,300),(3,'Diapers',5.00,400),(4,'Canned Beans',1.50,600),(5,'Rice',0.75,800),(6,'Toothpaste',2.00,350),(7,'Water Bottles',1.00,900),(8,'Blanket',7.00,200),(9,'First Aid Kit',15.00,150),(10,'Flashlight',10.00,100);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `package`
+--
+
+DROP TABLE IF EXISTS `package`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `package` (
+  `packageId` int NOT NULL AUTO_INCREMENT,
+  `taskCode` int NOT NULL,
+  `packageTypeId` int NOT NULL,
+  `packageCreateDate` date NOT NULL,
+  `packageWeight` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`packageId`),
+  KEY `taskCode` (`taskCode`),
+  KEY `packageTypeId` (`packageTypeId`),
+  CONSTRAINT `package_ibfk_1` FOREIGN KEY (`taskCode`) REFERENCES `task` (`taskCode`),
+  CONSTRAINT `package_ibfk_2` FOREIGN KEY (`packageTypeId`) REFERENCES `package_type` (`packageTypeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `package`
+--
+
+LOCK TABLES `package` WRITE;
+/*!40000 ALTER TABLE `package` DISABLE KEYS */;
+INSERT INTO `package` VALUES (1,1,1,'2025-03-01',5.50),(2,2,2,'2025-03-02',6.30),(3,3,3,'2025-03-03',4.75),(4,1,4,'2025-03-04',5.20),(5,2,5,'2025-03-05',5.80),(6,3,6,'2025-03-06',6.10),(7,1,7,'2025-03-07',5.30),(8,2,8,'2025-03-08',5.90),(9,3,9,'2025-03-09',6.20),(10,1,10,'2025-03-10',5.70),(11,2,2,'2025-03-02',6.30),(12,2,5,'2025-03-05',5.80);
+/*!40000 ALTER TABLE `package` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `package_contents`
+--
+
+DROP TABLE IF EXISTS `package_contents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `package_contents` (
+  `itemId` int NOT NULL,
+  `packageId` int NOT NULL,
+  `itemQuantity` int NOT NULL,
+  PRIMARY KEY (`itemId`,`packageId`),
+  KEY `packageId` (`packageId`),
+  CONSTRAINT `package_contents_ibfk_1` FOREIGN KEY (`itemId`) REFERENCES `item` (`itemId`),
+  CONSTRAINT `package_contents_ibfk_2` FOREIGN KEY (`packageId`) REFERENCES `package` (`packageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `package_contents`
+--
+
+LOCK TABLES `package_contents` WRITE;
+/*!40000 ALTER TABLE `package_contents` DISABLE KEYS */;
+INSERT INTO `package_contents` VALUES (1,1,10),(2,1,5),(3,2,8),(4,3,12),(5,3,10),(6,4,7),(7,5,15),(8,6,6),(9,7,4),(10,8,3);
+/*!40000 ALTER TABLE `package_contents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `package_type`
+--
+
+DROP TABLE IF EXISTS `package_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `package_type` (
+  `packageTypeId` int NOT NULL AUTO_INCREMENT,
+  `packageTypeName` varchar(100) NOT NULL,
+  PRIMARY KEY (`packageTypeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `package_type`
+--
+
+LOCK TABLES `package_type` WRITE;
+/*!40000 ALTER TABLE `package_type` DISABLE KEYS */;
+INSERT INTO `package_type` VALUES (1,'Basic Medical'),(2,'Child-Care'),(3,'Food'),(4,'Hygiene Kit'),(5,'Emergency Blanket'),(6,'First Aid'),(7,'Survival Kit'),(8,'Cooking Set'),(9,'Water Purification'),(10,'Clothing');
+/*!40000 ALTER TABLE `package_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `packing_list`
+--
+
+DROP TABLE IF EXISTS `packing_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `packing_list` (
+  `packingListId` int NOT NULL AUTO_INCREMENT,
+  `packingListName` varchar(100) NOT NULL,
+  `packingListDescription` text NOT NULL,
+  PRIMARY KEY (`packingListId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `packing_list`
+--
+
+LOCK TABLES `packing_list` WRITE;
+/*!40000 ALTER TABLE `packing_list` DISABLE KEYS */;
+INSERT INTO `packing_list` VALUES (1,'Basic Medical Supplies','Contains bandages, antiseptic, pain relievers'),(2,'Child-Care Kit','Contains diapers, baby food, formula, wipes'),(3,'Food Package','Contains canned goods, rice, pasta, water bottles');
+/*!40000 ALTER TABLE `packing_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task` (
+  `taskCode` int NOT NULL AUTO_INCREMENT,
+  `packingListId` int DEFAULT NULL,
+  `taskTypeId` int NOT NULL,
+  `taskStatusId` int NOT NULL,
+  `taskDescription` text NOT NULL,
+  PRIMARY KEY (`taskCode`),
+  KEY `packingListId` (`packingListId`),
+  KEY `taskTypeId` (`taskTypeId`),
+  KEY `taskStatusId` (`taskStatusId`),
+  CONSTRAINT `task_ibfk_1` FOREIGN KEY (`packingListId`) REFERENCES `packing_list` (`packingListId`),
+  CONSTRAINT `task_ibfk_2` FOREIGN KEY (`taskTypeId`) REFERENCES `task_type` (`taskTypeId`),
+  CONSTRAINT `task_ibfk_3` FOREIGN KEY (`taskStatusId`) REFERENCES `task_status` (`taskStatusId`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` VALUES (1,1,2,3,'Prepare 5000 medical supply packages'),(2,2,2,3,'Prepare 3000 child-care kits'),(3,3,2,3,'Prepare 4000 food packages'),(4,1,2,1,'Sort medical supplies'),(5,2,2,1,'Organize baby products'),(6,3,2,1,'Check expiration dates on food items'),(7,1,2,3,'Assemble first-aid kits'),(8,2,2,3,'Pack emergency diapers and formula'),(9,3,2,3,'Distribute food packages to shelters'),(10,1,2,3,'Label medical supply boxes');
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_status`
+--
+
+DROP TABLE IF EXISTS `task_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task_status` (
+  `taskStatusId` int NOT NULL AUTO_INCREMENT,
+  `taskStatusName` varchar(50) NOT NULL,
+  PRIMARY KEY (`taskStatusId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_status`
+--
+
+LOCK TABLES `task_status` WRITE;
+/*!40000 ALTER TABLE `task_status` DISABLE KEYS */;
+INSERT INTO `task_status` VALUES (1,'Ongoing'),(2,'Open'),(3,'Completed');
+/*!40000 ALTER TABLE `task_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_type`
+--
+
+DROP TABLE IF EXISTS `task_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task_type` (
+  `taskTypeId` int NOT NULL AUTO_INCREMENT,
+  `taskTypeName` varchar(50) NOT NULL,
+  PRIMARY KEY (`taskTypeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_type`
+--
+
+LOCK TABLES `task_type` WRITE;
+/*!40000 ALTER TABLE `task_type` DISABLE KEYS */;
+INSERT INTO `task_type` VALUES (1,'Recurring'),(2,'Packing');
+/*!40000 ALTER TABLE `task_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `volunteer`
+--
+
+DROP TABLE IF EXISTS `volunteer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `volunteer` (
+  `volunteerId` int NOT NULL AUTO_INCREMENT,
+  `volunteerName` varchar(100) NOT NULL,
+  `volunteerAddress` text NOT NULL,
+  `volunteerTelephone` varchar(15) NOT NULL,
+  PRIMARY KEY (`volunteerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `volunteer`
+--
+
+LOCK TABLES `volunteer` WRITE;
+/*!40000 ALTER TABLE `volunteer` DISABLE KEYS */;
+INSERT INTO `volunteer` VALUES (1,'Harry Potter','4 Privet Drive, Little Whinging','123-456-7890'),(2,'Hermione Granger','Hampstead, London','987-654-3210'),(3,'Ron Weasley','The Burrow, Ottery St. Catchpole','555-555-5555'),(4,'Albus Dumbledore','Hogwarts School, Scotland','444-444-4444'),(5,'Severus Snape','Spinner, Cokeworth','333-333-3333'),(6,'Minerva McGonagall','Hogwarts Castle, Scotland','222-222-2222'),(7,'Draco Malfoy','Malfoy Manor, Wiltshire','111-111-1111'),(8,'Luna Lovegood','Ottery St. Catchpole','666-666-6666'),(9,'Neville Longbottom','Hogsmeade, Scotland','777-777-7777'),(10,'Sirius Black','12 Grimmauld Place, London','888-888-8888');
+/*!40000 ALTER TABLE `volunteer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-02 11:49:56
